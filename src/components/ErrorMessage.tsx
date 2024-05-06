a15e47c15ff45e6409f0e59d00dcb4b1a9e3bc4e
Miguel Cobian <miguelcobian99@gmail.com>
2024-05-06 13:29:55 -0600

    Feat. Add form with React Hook Form

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react"
+
+export const ErrorMessage = ({ children }: { children: ReactNode }) => {
+    return (
+        <div className="text-center my-4 bg-red-100 text-red-600 font-bold p-3 uppercase text-sm">
+            {children}
+        </div>
+    )
+}
--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,19 +1,47 @@
+import ProjectForm from "@/components/projects/ProjectForm";
+import { useForm } from "react-hook-form"
 import { Link } from "react-router-dom"
 
 export const CreateProjectView = () => {
+
+    const initialValues = {
+        projectName: '',
+        clientName: '',
+        description: '',
+    }
+    const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues });
+
+    const handleForm = (data) => {
+        console.log(data);
+    }
+
     return (
         <>
-            <h1 className="text-5xl font-black">Crear proyecto</h1>
-            <p className="text-2xl font-light text-gray-500 mt-5">Completa el siguiente formulario para crear un proyecto</p>
+            <div className="max-w-3xl mx-auto">
+                <h1 className="text-5xl font-black">Crear proyecto</h1>
+                <p className="text-2xl font-light text-gray-500 mt-5">Completa el siguiente formulario para crear un proyecto</p>
 
-            <nav className="my-5">
-                <Link
-                    className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
-                    to="/"
+                <nav className="my-5">
+                    <Link
+                        className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+                        to="/"
+                    >
+                        Volver a proyectos
+                    </Link>
+                </nav>
+
+                <form
+                    className="mt-10 bg-white shadow-lg p-10 rounded-lg"
+                    onSubmit={handleSubmit(handleForm)}
+                    noValidate
                 >
-                    Volver a proyectos
-                </Link>
-            </nav>
+                    <ProjectForm register={register} errors={errors} />
+                    <input
+                        className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3 text-white uppercase font-bold cursor-pointer transition-colors"
+                        type="submit"
+                        value="Crear proyecto" />
+                </form>
+            </div>
         </>
     )
 }
