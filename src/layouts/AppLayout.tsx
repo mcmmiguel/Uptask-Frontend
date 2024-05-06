de9d3a572462a26f5ed2efa1373d3cc222ea9f6d
Miguel Cobian <miguelcobian99@gmail.com>
2024-05-06 12:38:10 -0600

    Chore. Eneable use of the base url

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,10 +1,28 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
+import { Logo } from "@/components/Logo";
 
 export const AppLayout = () => {
     return (
         <>
-            <div>AppLayout</div>
-            <Outlet />
+            <header className="bg-gray-800 py-5 ">
+                <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
+                    <div className="w-64">
+                        <Logo />
+                    </div>
+
+                    <nav>
+
+                    </nav>
+                </div>
+            </header>
+
+            <section className="max-w-screen-2xl mx-auto mt-10 p-5">
+                <Outlet />
+            </section>
+
+            <footer className="py-5">
+                <p className="text-center">Todos los derechos reservados {new Date().getFullYear()}</p>
+            </footer>
         </>
     )
 }
