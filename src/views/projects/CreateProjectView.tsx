a606b4f989ea0a113928b1eb2026281dd203872b
Miguel Cobian <miguelcobian99@gmail.com>
2024-05-06 13:01:51 -0600

    Feat. Add page for create a project

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom"
 
-export const DashboardView = () => {
+export const CreateProjectView = () => {
     return (
         <>
-            <h1 className="text-5xl font-black">Mis proyectos</h1>
-            <p className="text-2xl font-light text-gray-500 mt-5">Maneja y administra tus proyectos</p>
+            <h1 className="text-5xl font-black">Crear proyecto</h1>
+            <p className="text-2xl font-light text-gray-500 mt-5">Completa el siguiente formulario para crear un proyecto</p>
 
             <nav className="my-5">
                 <Link
                     className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
-                    to="/projects/create"
+                    to="/"
                 >
-                    Nuevo proyecto
+                    Volver a proyectos
                 </Link>
             </nav>
         </>
