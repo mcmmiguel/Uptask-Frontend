d953dff0e00b57b8a43fdb9cba687ccc5639795f
Miguel Cobian <miguelcobian99@gmail.com>
2024-05-06 19:43:35 -0600

    Feat. Get project to edit

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,5 +1,5 @@
 import api from "@/lib/axios";
-import { DashboardProjectSchema, ProjectFormData } from "../types";
+import { DashboardProjectSchema, Project, ProjectFormData } from "../types";
 import { isAxiosError } from "axios";
 
 export async function createProject(formData: ProjectFormData) {
@@ -21,6 +21,17 @@ export async function getProjects() {
         if (response.success) {
             return response.data;
         }
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error);
+        }
+    }
+}
+
+export async function getProjectById(id: Project['_id']) {
+    try {
+        const { data } = await api(`/projects/${id}`);
+        return data;
     } catch (error) {
         if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error);
--- src/views/projects/EditProjectView.tsx
+++ src/views/projects/EditProjectView.tsx
@@ -1,11 +1,19 @@
 import { useParams } from "react-router-dom"
+import { useQuery } from "@tanstack/react-query";
+import { getProjectById } from "@/api/ProjectAPI";
 
 export const EditProjectView = () => {
 
     const params = useParams();
     const projectId = params.projectId!;
 
-    console.log(projectId);
+    const { data, isLoading } = useQuery({
+        queryKey: ['editProject', projectId],
+        queryFn: () => getProjectById(projectId),
+        retry: false,
+    });
+
+    console.log(data);
 
     return (
         <div>EditProjectView</div>
