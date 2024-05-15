9453a14d41c5a0c9ab5e8267e7006399710c5c2a
Miguel Cobian <miguelcobian99@gmail.com>
2024-05-14 20:22:48 -0600

    Feat. Send a new confirmation code

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,5 +1,5 @@
 import { isAxiosError } from "axios";
-import { ConfirmToken, UserRegistrationForm } from "../types";
+import { ConfirmToken, RequestConfirmationCodeForm, UserRegistrationForm } from "../types";
 import api from "@/lib/axios";
 
 export async function createAccount(formData: UserRegistrationForm) {
@@ -17,6 +17,18 @@ export async function createAccount(formData: UserRegistrationForm) {
 export async function confirmAccount(formData: ConfirmToken) {
     try {
         const url = '/auth/confirm-account';
+        const { data } = await api.post<string>(url, formData);
+        return data;
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error);
+        }
+    }
+}
+
+export async function requestConfirmationCode(formData: RequestConfirmationCodeForm) {
+    try {
+        const url = '/auth/request-code';
         const { data } = await api.post<string>(url, formData);
         return data;
     } catch (error) {
