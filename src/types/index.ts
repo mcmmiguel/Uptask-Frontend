15ff8e151201fbd7f064559108d33a3df34f45d3
Miguel Cobian <miguelcobian99@gmail.com>
2024-05-14 18:44:52 -0600

    Feat. Add login page

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,16 @@
 import { z } from 'zod';
 
+// Auth & Users
+const authSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string(),
+    password_confirmation: z.string(),
+});
+
+type Auth = z.infer<typeof authSchema>;
+export type UserLoginForm = Pick<Auth, 'email' | 'password'>;
+
 // Tasks
 export const TaskStatusSchema = z.enum(["pending", "hold", "inProgress", "underReview", "completed"]);
 
