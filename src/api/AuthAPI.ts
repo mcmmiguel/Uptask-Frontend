c906ef5f3056ef8065841f46aa358e9fab65c1cf
Miguel Cobian <miguelcobian99@gmail.com>
2024-05-20 14:21:11 -0600

    Chore. Define user schema

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,5 +1,5 @@
 import { isAxiosError } from "axios";
-import { ConfirmToken, ForgotPasswordForm, NewPasswordForm, RequestConfirmationCodeForm, UserLoginForm, UserRegistrationForm } from "../types";
+import { ConfirmToken, ForgotPasswordForm, NewPasswordForm, RequestConfirmationCodeForm, UserLoginForm, UserRegistrationForm, userSchema } from "../types";
 import api from "@/lib/axios";
 
 export async function createAccount(formData: UserRegistrationForm) {
@@ -90,7 +90,8 @@ export async function updatePasswordWithToken({ formData, token }: { formData: N
 export async function getUser() {
     try {
         const { data } = await api('/auth/user');
-        return data;
+        const response = userSchema.safeParse(data);
+        if (response.success) return response.data;
     } catch (error) {
         if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error);
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -16,7 +16,7 @@ export const AppLayout = () => {
         return <Navigate to={'/auth/login'} />
     }
 
-    return (
+    if (data) return (
         <>
             <header className="bg-gray-800 py-5 ">
                 <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
