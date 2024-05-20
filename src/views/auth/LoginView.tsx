0381c39c9caad5a4649b6dc9785b602238cd96df
Miguel Cobian <miguelcobian99@gmail.com>
2024-05-20 14:39:58 -0600

    Fix. Delete toast from login

--- src/views/auth/LoginView.tsx
+++ src/views/auth/LoginView.tsx
@@ -22,7 +22,6 @@ export default function LoginView() {
             toast.error(error.message);
         },
         onSuccess: () => {
-            toast.success('Iniciando sesión...');
             navigate('/');
         }
     })
