96a5580e1985e59ffc63831e815575f3b139b80e
Miguel Cobian <miguelcobian99@gmail.com>
2024-05-20 13:15:58 -0600

    Fix. Get projects

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -14,8 +14,14 @@ export async function createProject(formData: ProjectFormData) {
 }
 
 export async function getProjects() {
+    const token = localStorage.getItem('AUTH_TOKEN');
     try {
-        const { data } = await api('/projects');
+        const { data } = await api('/projects', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+        );
         const response = DashboardProjectSchema.safeParse(data);
 
         if (response.success) {
--- src/views/auth/LoginView.tsx
+++ src/views/auth/LoginView.tsx
@@ -1,12 +1,14 @@
 import { useForm } from "react-hook-form";
 import { UserLoginForm } from "@/types/index";
 import { ErrorMessage } from "@/components/ErrorMessage";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 import { authenticateUser } from "@/api/AuthAPI";
 import { toast } from "react-toastify";
 
 export default function LoginView() {
+
+    const navigate = useNavigate();
 
     const initialValues: UserLoginForm = {
         email: '',
@@ -21,6 +23,7 @@ export default function LoginView() {
         },
         onSuccess: () => {
             toast.success('Iniciando sesión...');
+            navigate('/');
         }
     })
 
