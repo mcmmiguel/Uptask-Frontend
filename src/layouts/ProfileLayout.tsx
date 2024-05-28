c3f16cd7b5b2d4a58a7d7452b378eba93547dfb5
Miguel Cobian <miguelcobian99@gmail.com>
2024-05-27 18:57:07 -0600

    Feat. Add tabs

--- src/layouts/ProfileLayout.tsx
+++ src/layouts/ProfileLayout.tsx
@@ -1,9 +1,10 @@
+import Tabs from "@/components/profile/Tabs"
 import { Outlet } from "react-router-dom"
 
 export const ProfileLayout = () => {
     return (
         <>
-            <div>ProfileLayout</div>
+            <Tabs />
 
             <Outlet />
         </>
