2bce052bb041057a4a7243e934e13a34b415d259
Miguel Cobian <miguelcobian99@gmail.com>
2024-05-27 19:08:34 -0600

    Feat. Add Profile Form

--- src/views/profile/ProfileView.tsx
+++ src/views/profile/ProfileView.tsx
@@ -1,5 +1,11 @@
+import ProfileForm from "@/components/profile/ProfileForm";
+import { useAuth } from "@/hooks/useAuth"
+
 export const ProfileView = () => {
-    return (
-        <div>ProfileView</div>
-    )
+
+    const { data, isLoading } = useAuth();
+
+    if (isLoading) return 'Cargando...';
+
+    if (data) return <ProfileForm data={data} />
 }
